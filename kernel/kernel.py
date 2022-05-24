d6ac0982cd1f542852b3e2d01ad602cbe51d233d
oscar12344456 <chen.oscar@hotmail.com>
2022-05-24 17:27:02 +0800

    [Add]add defined kernel

--- kernel/kernel.py
+++ kernel/kernel.py
@@ -1,4 +1,16 @@
 import numpy as np
+from scipy.spatial.distance import pdist, squareform
 
-def defined_kernel():
-    pass
+
+def defined_kernel(X, gamma_s, gamma_c):
+    # kernel: 10000 * 10000
+    data_number = X.shape[0]
+    S = np.zeros((data_number, 2))  # S: the coordinate of the pixel
+    for i in range(data_number):
+        S[i, 0] = i // 100
+        S[i, 1] = i % 100
+    S_ = squareform(np.exp((-gamma_s) * pdist(S, 'sqeuclidean')))
+    C_ = squareform(np.exp((-gamma_c) * pdist(X, 'sqeuclidean')))
+    kernel = S_ * C_
+
+    return kernel
--- main_kernel_kmeans.py
+++ main_kernel_kmeans.py
@@ -1,18 +1,20 @@
 import numpy as np
 from dataloader.dataloader import image_loader
+from kernel.kernel import defined_kernel
+
+# config
+gamma_s = 1e-3
+gamma_c = 1e-3
 
 # load images data
+pic, height, weight = image_loader("image1.png")  # pic:(10000, 3)
+
 # design kernel (with announcement)
 # using kernel get gram matrix
+gram = defined_kernel(pic, gamma_s, gamma_c)
+
 # kmeans function (diff types initialization)
 # using kmeans function to get the clustering number
 # plot the heatmaps
-
-
-pic, height, weight = image_loader("image1.png")  # pic:(10000, 3)
-
-
-
-
 
 print()
